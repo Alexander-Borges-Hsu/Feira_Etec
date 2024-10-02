c07a6aaf898f510c2871e82b03cfc03c66431317
Carlosh088 <carlosh202208@Gmail.com>
2024-10-02 16:32:09 -0300

    1.0.0.6 Front

---
+++ FeiraTecnologica/src/views/script.js
@@ -0,0 +1,12 @@
+function formatCurrency(input) {
+    let value = input.value.replace(/\D/g, "");
+    value = (value / 100).toFixed(2); 
+    input.value = value.toString().replace(".", ","); 
+}
+document.addEventListener("DOMContentLoaded", function() {
+    const moneyInput = document.getElementById("moneyInput");
+ 
+    moneyInput.addEventListener("input", function() {
+        formatCurrency(moneyInput);
+    });
+});
