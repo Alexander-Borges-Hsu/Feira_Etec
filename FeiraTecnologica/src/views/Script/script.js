35434d9a926cefd437c3c6f71c363c65bb85e1c3
Carlosh088 <carlosh202208@Gmail.com>
2024-10-03 12:24:48 -0300

    1.0.0.9

--- FeiraTecnologica/src/views/Script/script.js
+++ FeiraTecnologica/src/views/Script/script.js
@@ -11,3 +11,44 @@ document.addEventListener("DOMContentLoaded", function() {
         formatCurrency(moneyInput);
     });
 });
+
+/*O código acima foi feito para permitir que somente valores monetários sejam inseridos no form1, em consumo de energia*/
+
+const carroBtn = document.getElementById('carro-btn');
+       
+            const motoBtn = document.getElementById('moto-btn')
+       
+            let transporteSelecionado = "";
+       
+            carroBtn.addEventListener('click', function() {
+              transporteSelecionado = 'Carro';
+              carroBtn.classList.add('ativo');
+              motoBtn.classList.remove('ativo')
+       
+              motoBtn.addEventListener('click', function(){
+                transporteSelecionado = 'Moto';
+                motoBtn.classList.add('ativo');
+                carroBtn.classList.remove('ativo');
+              })
+            })
+            document.getElementById('formulario').addEventListener('submit', function(event) {
+          event.preventDefault();
+         
+          const kms = document.getElementById('kms').value;
+          const dias = document.getElementById('dias').value;
+          const combustivel = document.getElementById('combustivel').checked;
+         
+          if (kms && dias) {
+              alert(`Você rodou ${kms} kms em ${dias} dias. Combustível: ${combustivel ? 'Sim' : 'Não'}.`);
+          } else {
+              alert('Preencha todos os campos.');
+          }
+      });
+
+      /* O Código acima foi feito pelo Davi*/
+
+      document.getElementById("integerInput").addEventListener("input", function() {
+        this.value = this.value.replace(/[^0-9]/g, ''); // Remove qualquer caractere não numérico
+    });
+
+    /*O código acima foi feito para somente válidar numeros inteiros na página form1 em gás natural e em maços de cigarro*/
