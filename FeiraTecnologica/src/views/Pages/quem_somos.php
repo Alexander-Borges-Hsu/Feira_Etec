bfe1f6f59320277e7e1b60e46ec7698eeb7ec58d
LOBOPIDAODEV <gustavosupremebatera@gmail.com>
2024-11-05 19:31:12 -0300

    subindo o Quem Somos

--- FeiraTecnologica/src/views/Pages/quem_somos.php
+++ FeiraTecnologica/src/views/Pages/quem_somos.php
@@ -1,38 +1,118 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="FeiraTecnologica\src\views\Style\styleSomos.css">
+    <link rel="stylesheet" href="../Style/styleSomos.css">
 </head>
+
 <body>
-<?php 
-    include('C:\Users\gusta\Feira_Etec\FeiraTecnologica\src\views\AppView\NavBar.php');
-  ?>
 
-<main>
-    <center><img class="apresentando" src="imgtopocorreto.jpg" width="90%" height=""></center>
-    <section class="topo-do-site">
-        <div class="interface">
-            <div class="flex">
-                <div class="txt-topo-site">
-                    <h1>CONHEÇA OS DESENVOLVEDORES E <br>COLABORADORES DA <span>VERDE</span>CALE:</h1><br>
-                    <P>Esse site foi desenvolvido por um grupo de alunos da etec zona leste que tem como objetivo, informar,<br> ajudar e conscientizar
-                     as pessoas em relação a emissão de dióxido de carbono que emitimmos no <br> dia à dia através do uso de transporte automobilistico.</P>
-                </div>
+    <?php
+    include('../AppView/NavBar.php');
+    ?>
 
-                <div class="img-topo-site">
-                     <img src="SCCP_escudo-150px.png" width="480px" alt="">
-                </div>
+
+    <div class="centered">
+        <div class="loader">
+            <p>VERDE<span class="calc">CALC.</span></p>
+            <div class="words">
+                <span class="word">reciclagem</span>
+                <span class="word">natureza</span>
+                <span class="word">conservação</span>
+                <span class="word">ecossistema</span>
+                <span class="word">preservação</span>
             </div>
-        </div><!--interface-->
-    </section><!--topo da interface-->
-</main>
+        </div>
+    </div>
 
 
-<?php
+
+
+    <main>
+        <center><img class="apresentando" src="../imagens/imgtopocorreto.jpg" width="90%" height=""></center> <br><br>
+        <section class="topo-do-site">
+            <div class="interface">
+                <div class="flex">
+                    <div class="txt-topo-site">
+                        <h3> Somos a VerdeCalc! E nossa equipe é formada por um grupo de 6 estudantes da Etec Zona-Leste, (cidade de São Paulo,
+                            Brasil), sendo eles: Alexander Borges, Davi Lucas, Carlos Henrique, Gustavo Ribeiro, Kauan Souza e Diego Oliveira;
+                            que em conjunto pensamos em como poderíamos ajudar o meio ambiente e ajudar as pessoas que se preocupam com o mesmo.
+                        </h3>
+
+                        <h3>O consumo residencial de eletricidade, gás de cozinha e cigarro representa uma importante fonte de poluição do ar e impactos ambientais. A demanda por eletricidade em casas e apartamentos gera uma necessidade constante de produção de energia, que muitas vezes provém de fontes não-renováveis, como usinas a carvão, gás natural e petróleo. Essas fontes emitem uma grande quantidade de dióxido de carbono (CO₂) e outros poluentes na atmosfera, contribuindo para o aquecimento global e a degradação da qualidade do ar. Nas residências, o uso de aparelhos elétricos em grande escala, como aquecedores, ar-condicionado e eletrodomésticos, amplifica essa demanda e, por consequência, os impactos ambientais. <br><br>
+
+                            O uso de gás de cozinha (GLP), bastante comum em residências para o preparo de alimentos, também possui um efeito poluente, embora em menor escala. Quando o gás é queimado, ele libera gases de combustão, como CO₂, que contribuem para o efeito estufa. Além disso, em situações onde o gás não é totalmente queimado, podem ser liberados monóxido de carbono (CO) e outros hidrocarbonetos, que afetam a saúde e a qualidade do ar dentro das casas, especialmente em áreas mal ventiladas. <br><br>
+
+                            Já o cigarro, por sua vez, é uma fonte de poluição residencial menos evidente, mas significativa. A queima do tabaco libera uma série de substâncias tóxicas, como nicotina, alcatrão e monóxido de carbono, que impactam diretamente a saúde dos fumantes e das pessoas ao redor, especialmente em ambientes fechados. Esses poluentes também se acumulam no ambiente, aderindo a superfícies e tecidos, o que prolonga a exposição de todos aos resíduos tóxicos.</h3>
+
+                        <h3>
+                            Pensando nisso... em um curto períódo de 4 meses de muito trabalho, a equipe VerdeCalc desenvolveu esse site com o objetivo
+                            de através da tecnologia e da matemática ajudar as pessoas a se conscientizar e preservar o meio ambiente diminuindo
+                            sua emissão de dióxido de carbono semanal.
+                        </h3>
+                    </div>
+
+                    <div class="img-topo-site">
+                        <img src="../imagens/SCCP_escudo-150px.png" width="480px" alt="">
+                    </div>
+                          
+                </div><br><br><br><br>
+
+                <div class="centered">
+        <div class="loader">
+            <p>CALCULO DE <span class="calc"> VEÍCULOS</span></p>
+            <div class="words">
+                <span class="word">diesel</span>
+                <span class="word">etanol</span>
+                <span class="word">carro</span>
+                <span class="word">caminhão</span>
+                <span class="word">moto</span>
+            </div>
+        </div>
+    </div>
+<br>
+    <div class="txt-topo-site">
+                        <h3> Claro que não tratamos apenas de gás de cozinha, eletrecidade e cigarro... até por que em média a emissão de gases poluentes
+                            desses três juntos ainda são menores em relaçao a emissão que um veículo à gasolina comum emite; pensando nessa informação
+                            importantíssima, fizemos também a calculadora de veículos! No páragrafo abaixo falamos um pouco sobre o quão grave é a emissão de gases
+                            poluentes por veículos como moto, carro e caminhão:
+                        </h3>
+
+                        <h3>O consumo residencial de eletricidade, gás de cozinha e cigarro representa uma importante fonte de poluição do ar e impactos ambientais. A demanda por eletricidade em casas e apartamentos gera uma necessidade constante de produção de energia, que muitas vezes provém de fontes não-renováveis, como usinas a carvão, gás natural e petróleo. Essas fontes emitem uma grande quantidade de dióxido de carbono (CO₂) e outros poluentes na atmosfera, contribuindo para o aquecimento global e a degradação da qualidade do ar. Nas residências, o uso de aparelhos elétricos em grande escala, como aquecedores, ar-condicionado e eletrodomésticos, amplifica essa demanda e, por consequência, os impactos ambientais. <br><br>
+
+                            O uso de gás de cozinha (GLP), bastante comum em residências para o preparo de alimentos, também possui um efeito poluente, embora em menor escala. Quando o gás é queimado, ele libera gases de combustão, como CO₂, que contribuem para o efeito estufa. Além disso, em situações onde o gás não é totalmente queimado, podem ser liberados monóxido de carbono (CO) e outros hidrocarbonetos, que afetam a saúde e a qualidade do ar dentro das casas, especialmente em áreas mal ventiladas. <br><br>
+
+                            Já o cigarro, por sua vez, é uma fonte de poluição residencial menos evidente, mas significativa. A queima do tabaco libera uma série de substâncias tóxicas, como nicotina, alcatrão e monóxido de carbono, que impactam diretamente a saúde dos fumantes e das pessoas ao redor, especialmente em ambientes fechados. Esses poluentes também se acumulam no ambiente, aderindo a superfícies e tecidos, o que prolonga a exposição de todos aos resíduos tóxicos.</h3>
+
+                        <h3>
+                            Pensando nisso... em um curto períódo de 4 meses de muito trabalho, a equipe VerdeCalc desenvolveu esse site com o objetivo
+                            de através da tecnologia e da matemática ajudar as pessoas a se conscientizar e preservar o meio ambiente diminuindo
+                            sua emissão de dióxido de carbono semanal.
+                        </h3>
+                    </div>
+
+            </div><!--interface-->
+        </section><!--topo da interface-->
+
+        <br><br><br><br>
+
+        <article>
+            <div class="visao">
+                <img src="../imagens/3040659.png" width="280px" alt="">
+            </div>
+        </article>
+
+
+    </main>
+
+    <br><br><br><br>
+
+    <?php
     include('C:\Users\gusta\Feira_Etec\FeiraTecnologica\src\views\AppView\Footer.php');
     ?>
 </body>
+
 </html>
