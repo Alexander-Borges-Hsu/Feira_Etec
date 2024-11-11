1b6fb9fb7ae6f03f335f4b3964f7df68bb8e50c5
carlosh088 <carlosh202208@Gmail.com>
2024-11-11 15:06:58 -0300

    1.0.4.7

--- FeiraTecnologica/src/views/Pages/form1.php
+++ FeiraTecnologica/src/views/Pages/form1.php
@@ -19,36 +19,39 @@
   <?php 
   include('../AppView/NavBar.php');
 ?>
-  <main class="container">
+ <main class="container">
     <h1 class="text-center mb-4">Consumo de Energia e Recursos</h1>
     
-    <div class="row justify-content-center">
-      <section class="col-12 col-md-6 col-lg-4 consumo">
-        <h2>Consumo de Eletricidade</h2>
-        <form action="../../../index.php" method="POST">
-        <input type="text" name="k" value="CCL" style="display: none;">
-          <label for="moneyInput">Digite seu consumo mensal de energia:</label>
-          <input type="text" id="moneyInput" placeholder="R$" required name="eletricidade">
-      </section>
-      
-      <section class="col-12 col-md-6 col-lg-4 consumo">
-        <h2>Consumo de Gás Natural</h2>
-        <label for="gasInput">Quantos botijões de gás você utiliza por mês?</label>
-        <input type="number" id="gasInput" placeholder="Digite um número" min="0" step="1" required name="butijoes">
-      </section>
-      
-      <section class="col-12 col-md-6 col-lg-4 consumo">
-        <h2>Consumo Próprio</h2>
-        <label for="smokeInput">Quantos maços de cigarro você consome por semana?</label>
-        <input type="number" id="smokeInput" placeholder="Digite um número" min="0" step="1" required name="cigarro">
-      </section>
+    <div class="form-content">
+        <div class="row justify-content-center">
+            <section class="col-12 col-md-6 col-lg-4 consumo">
+                <h2>Consumo de Eletricidade</h2>
+                <form action="../../../index.php" method="POST">
+                    <input type="text" name="k" value="CCL" style="display: none;">
+                    <label for="moneyInput">Digite seu consumo mensal de energia:</label>
+                    <input type="text" id="moneyInput" placeholder="R$" required name="eletricidade">
+                </section>
+
+                <section class="col-12 col-md-6 col-lg-4 consumo">
+                    <h2>Consumo de Gás Natural</h2>
+                    <label for="gasInput">Quantos botijões de gás você utiliza por mês?</label>
+                    <input type="number" id="gasInput" placeholder="Digite um número" min="0" step="1" required name="butijoes">
+                </section>
+
+                <section class="col-12 col-md-6 col-lg-4 consumo">
+                    <h2>Consumo Próprio</h2>
+                    <label for="smokeInput">Quantos maços de cigarro você consome por semana?</label>
+                    <input type="number" id="smokeInput" placeholder="Digite um número" min="0" step="1" required name="cigarro">
+                </section>
+            </div>
+
+            <div class="text-center mt-3">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </form>
     </div>
-    
-    <div class="text-center mt-3">
-      <button type="submit" class="btn btn-primary">Enviar</button>
-    </div>
-</form>
-  </main>
+</main>
+
   <?php
     include('../AppView/Footer.php');
     ?>
