6ae516d1f900d3722c0f821c6af10cff573bd186
carlosh088 <carlosh202208@Gmail.com>
2024-11-20 21:06:04 -0300

    1.0.5.6

---
+++ FeiraTecnologica/src/views/Pages/quem_somos(antigo).php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="./../Style/style_navbar.css">
+
+    <link rel="icon" href=".\..\imagens\imagem.png">
+    <link rel="stylesheet" href="..\Style\styleSomos.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <script src="../Script/script.js"></script>
+
+
+    <?php
+    include('../AppView/NavBar.php');
+    ?>
+
+
+
+</head>
+
+<body>
+
+    <br><br>
+
+
+    <div class="centered">
+        <div class="loader">
+            <p>VERDE<span class="calc">CALC.</span></p>
+            <div class="words">
+                <span class="word">reciclagem</span>
+                <span class="word">natureza</span>
+                <span class="word">conservação</span>
+                <span class="word">ecossistema</span>
+                <span class="word">preservação</span>
+            </div>
+        </div>
+    </div>
+    <main>
+        <center><img class="apresentando" src="../imagens/imgtopocorreto.jpg" width="90%" height=""></center> <br><br>
+        <section class="topo-do-site">
+            <div class="interface">
+                <div class="flex">
+                    <div class="txt-topo-site"><!--texto intodutório-->
+                        <h3> Somos a VerdeCalc! E nossa equipe é formada por um grupo de 6 estudantes da Etec
+                            Zona-Leste, (cidade de São Paulo,
+                            Brasil), sendo eles: Alexander Borges, Davi Lucas, Carlos Henrique, Gustavo Ribeiro, Kauan
+                            Souza e Diego Oliveira;
+                            que em conjunto pensamos em como poderíamos ajudar o meio ambiente e ajudar as pessoas que
+                            se preocupam com o mesmo.
+                        </h3>
+
+                        <h3><br>
+
+                            O uso de gás de cozinha (GLP), bastante comum em residências para o preparo de alimentos,
+                            também possui um efeito poluente, embora em menor escala. Quando o gás é queimado, ele
+                            libera gases de combustão, como CO₂, que contribuem para o efeito estufa. Além disso, em
+                            situações onde o gás não é totalmente queimado, podem ser liberados monóxido de carbono (CO)
+                            e outros hidrocarbonetos, que afetam a saúde e a qualidade do ar dentro das casas,
+                            especialmente em áreas mal ventiladas. <br><br>
+
+                            Já o cigarro, por sua vez, é uma fonte de poluição residencial menos evidente, mas
+                            significativa. A queima do tabaco libera uma série de substâncias tóxicas, como nicotina,
+                            alcatrão e monóxido de carbono, que impactam diretamente a saúde dos fumantes e das pessoas
+                            ao redor, especialmente em ambientes fechados. Esses poluentes também se acumulam no
+                            ambiente, aderindo a superfícies e tecidos, o que prolonga a exposição de todos aos resíduos
+                            tóxicos.</h3>
+
+                        <h3>
+                            Pensando nisso... em um curto períódo de 4 meses de muito trabalho, a equipe VerdeCalc
+                            desenvolveu esse site com o objetivo
+                            de através da tecnologia e da matemática ajudar as pessoas a se conscientizar e preservar o
+                            meio ambiente diminuindo
+                            sua emissão de dióxido de carbono semanal.
+                        </h3>
+                    </div>
+
+                    <div class="img-topo-site">
+                        <img src="../imagens/SCCP_escudo-150px.png" width="480px" alt=""><!--logo flutuante-->
+                    </div>
+
+                </div><br><br><br><br>
+
+                <div class="centered">
+                    <div class="loader">
+                        <p>CALCULO DE <span class="calc"> VEÍCULOS</span></p>
+                        <div class="words">
+                            <span class="word">diesel</span>
+                            <span class="word">etanol</span>
+                            <span class="word">carro</span>
+                            <span class="word">caminhão</span>
+                            <span class="word">moto</span>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="visao"><!--placas flutuantes-->
+                    <img src="..\imagens\placa-carro.png" width="280px">
+                    <img src="..\imagens\placa-caminhão.png" alt="" width="268px">
+                    <img src="..\imagens\placa-moto.png" alt="" width="280px">
+                </div>
+
+                <div class="txt-topo-site"><!--texto sobre veículos-->
+                    <h3> Claro que não tratamos apenas de gás de cozinha, eletrecidade e cigarro... até por que em média
+                        a emissão de gases poluentes
+                        desses três juntos ainda são menores em relaçao a emissão que um veículo à gasolina comum emite;
+                        pensando nessa informação
+                        importantíssima, fizemos também a calculadora de veículos! No páragrafo abaixo falamos um pouco
+                        sobre o quão grave é a emissão de gases
+                        poluentes por veículos como moto, carro e caminhão:
+                    </h3>
+
+                    <h3> A emissão de gases poluentes por veículos a combustão interna é uma das principais fontes de
+                        poluição atmosférica nas áreas urbanas. Esses veículos, que utilizam combustíveis fósseis como
+                        gasolina, diesel e etanol, liberam diversos compostos prejudiciais ao meio ambiente, como
+                        monóxido de carbono (CO), óxidos de nitrogênio (NOx) e hidrocarbonetos (HC). Esses poluentes
+                        contribuem diretamente para o aumento da poluição do ar, afetando tanto a qualidade do ar que
+                        respiramos quanto o equilíbrio natural do ecossistema.
+                        <br> <br>Além do impacto na qualidade do ar, os gases emitidos pelos veículos a combustão são
+                        grandes responsáveis pela intensificação do efeito estufa. O dióxido de carbono (CO₂), um dos
+                        subprodutos da queima de combustíveis fósseis, é um gás de efeito estufa que contribui para o
+                        aquecimento global, uma vez que ele se acumula na atmosfera e impede que o calor irradiado pela
+                        Terra escape para o espaço. Esse acúmulo de calor altera padrões climáticos, promovendo eventos
+                        climáticos extremos e colocando em risco a biodiversidade e os recursos naturais.
+                        <br><br>
+                        Por fim, os poluentes emitidos pelos veículos a combustão causam impactos significativos à saúde
+                        humana. A exposição prolongada a partículas e gases tóxicos no ar pode agravar ou desencadear
+                        problemas respiratórios, como asma e bronquite, além de aumentar o risco de doenças
+                        cardiovasculares. Em áreas urbanas com alto tráfego, onde a concentração de poluentes é maior,
+                        esses riscos são mais elevados, impactando especialmente crianças, idosos e pessoas com
+                        condições de saúde preexistentes.
+                    </h3>
+
+                    <br><br><br><br>
+
+                </div>
+
+            </div><!--interface-->
+
+
+        </section><!--topo da interface-->
+
+
+        <br><br><br><br>
+
+        <article>
+            <div class="box1">
+                <div class="visao"><!--logos inspiratórias finais-->
+                    <img src="../imagens/3040659.png" width="280px" alt="">
+                </div>
+
+                <h2 class="proposito">PROPÓSITO</h2><br>
+                <h3 class="textinho">Promover uma novo estilo de vida sustentável na vida <br> das pessoas com
+                    tecnologia, matemática e conscientização que abri<br>caminhos inovadores para a conservação e a
+                    redução da poluição do ar.</h3>
+            </div>
+
+            <div class="box2">
+                <img src="..\imagens\olho.png" alt="" width="280px" class="visao">
+                <h2 class="impacto">IMPACTO</h2><br>
+                <h3 class="impacto-txt">Promover uma novo estilo de vida sustentável na vida <br> das pessoas com
+                    tecnologia, matemática e conscientização que abri<br>caminhos inovadores para a conservação e a
+                    redução da poluição do ar.</h3>
+            </div>
+        </article>
+
+
+
+    </main>
+
+    <br><br><br><br>
+
+    <?php
+    include('../AppView/Footer.php');
+    ?>
+
+</body>
+
+</html>
